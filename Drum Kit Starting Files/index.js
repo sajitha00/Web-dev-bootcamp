da031346166ca7d9ebc4532aeef802d24d324214
Sajitha Geevinda Kodithuwakku <115799454+sajitha00@users.noreply.github.com>
2023-07-25 18:29:15 +0530

    d

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,10 +1,12 @@
 document.querySelector("button.w").addEventListener("click", function () {
   var audio = new Audio("sounds/tom-1.mp3");
   audio.play();
+  activeBtn();
 });
 document.querySelector("button.a").addEventListener("click", function () {
   audio = new Audio("sounds/tom-2.mp3");
   audio.play();
+  activeBtn();
 });
 document.querySelector("button.s").addEventListener("click", function () {
   audio = new Audio("sounds/tom-3.mp3");
@@ -40,33 +42,49 @@ function keypress() {
     if (event.key === "w") {
       var audio = new Audio("sounds/tom-1.mp3");
       audio.play();
+      // activeBtn();
     }
     if (event.key === "a") {
       audio = new Audio("sounds/tom-2.mp3");
       audio.play();
+      //activeBtn();
     }
     if (event.key === "s") {
       audio = new Audio("sounds/tom-3.mp3");
       audio.play();
+      //activeBtn();
     }
     if (event.key === "d") {
       audio = new Audio("sounds/tom-4.mp3");
       audio.play();
+      //activeBtn();
     }
     if (event.key === "j") {
       audio = new Audio("sounds/snare.mp3");
       audio.play();
+      //activeBtn();
     }
     if (event.key === "k") {
       audio = new Audio("sounds/crash.mp3");
       audio.play();
+      //activeBtn();
     }
     if (event.key === "l") {
       audio = new Audio("sounds/kick-bass.mp3");
       audio.play();
+      //activeBtn();
     } else {
       console.log("please enter valid key");
     }
   });
 }
+function activeBtn() {
+  document.querySelector(".drum").classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector(".drum").classList.remove("pressed");
+  }, 100);
+}
+
 keypress();
+
+//activeBtn();
