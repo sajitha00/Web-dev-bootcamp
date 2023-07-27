7e5a94105d7b24e306c1bdcd9361d356a164ce22
Sajitha Geevinda Kodithuwakku <115799454+sajitha00@users.noreply.github.com>
2023-07-27 22:53:11 +0530

    add

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,1 +1,8 @@
-$("h1").css("color", "red");
+// $("h1").css("color", "red");
+// $("h1").text("Good");
+// $("h1").html("<i>Hello</i>");
+// $("button.one").css("color","blue");
+$("input").keypress(function(event){
+     console.log(event.key);
+     $("h1").text(event.key);
+});
